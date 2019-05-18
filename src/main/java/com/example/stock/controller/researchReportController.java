ac0a5c84ea455182174fa016600239f7a794c9d0
TrueWWT <2741020164@qq.com>
2019-05-18 15:30:50 +0800

    研究报告（貌似叫这个？）researchReportcontroller完成

--- src/main/java/com/example/stock/controller/researchReportController.java
+++ src/main/java/com/example/stock/controller/researchReportController.java
@@ -1,5 +1,6 @@
 package com.example.stock.controller;
 
+import com.example.stock.Entity.Analysis;
 import com.example.stock.dataservice.DataMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 /*
  * 某一支具体股票的     研究报告
  * 前端提供             ts_code
- * 后端返回值           analysis对象
+ * 后端返回值           analysis对象Arraylist
  * url                  /research
  * 方法                 POST
  * */
@@ -21,11 +22,10 @@ public class researchReportController {
     private DataMethod dataMethod;
     @RequestMapping(value="/research",method = RequestMethod.POST)
     @ResponseBody
-    public Object getResearchReport(@RequestParam("ts_code")String ts_code){
-        Object res=new Object();//等待实体类和方法接口
+    public ArrayList<Analysis> getResearchReport(@RequestParam("ts_code")String ts_code){
+        ArrayList<Analysis>res=new ArrayList<>();//等待实体类和方法接口
         /*读取相关文件接口 赋值res*/
-        //res=dataMethod.getAllstocks();
-
+        res=dataMethod.findAnalysis(ts_code);
         return res;
     }
 }
